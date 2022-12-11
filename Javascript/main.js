4b0df97c9402ddf91d5ae8dfc51ec120607191b0
Jarod Calderon <jarodcalde97@gmail.com>
2022-12-10 22:57:46 -0300

    preEntregaJS2

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,19 +1,50 @@
-let contratar = prompt('¿Quieres promocionar un tema?')
+const precio = 200;
+const tax = 0.19;
 
-if (contratar == 'si') {
-    const precio = 200;
-    let temas = parseFloat(prompt('Cuantos temas quieres promocionar? máximo 3'));
+let contratar = prompt('¿Quieres promocionar un tema con nosotros?')
 
-    if (temas == 1) {
-        alert(`'El valor es ${precio}'`);
-    } else if (temas == 2) {
-        alert(`El precio es ${precio * 2}`);
-    } else if (temas == 3) {
-        alert(`El precio es ${precio * 3}`);
-    } else {
-        alert('Consigue mas informacion en la web')
+if (contratar.toLowerCase() == 'si') {
+    const persona = {
+        nombre: String,
+        apellido: String,
+        telefono: parseInt,
+        cantidadTemas: parseInt
     }
+
+    let precios = [precio, precio * 2, precio * 3]
+
+    persona.nombre = prompt("Cuál es tu nombre?")
+    persona.apellido = prompt("cuál es tu apellido?")
+    persona.telefono = prompt("Cuál es tu número telefónico")
+    persona.cantidadTemas = prompt("cuántos temas quieres promocionar, máximo 3")
+
+
+
+    const precioTema = precios.filter((precio,index) => {
+        if (
+            index == persona.cantidadTemas - 1
+        ) {
+            return true;
+        }
+        return false;
+    })
+
+    console.log(precioTema[0] * tax)
+    console.log(precioTema)
+
+    let iva = precioTema[0] * tax 
+    let total = precioTema[0] + iva
+    alert(`el precio de promocion de tus productos es ${total}`)
+    alert(`serás contactado al ${persona.telefono}`)
+
+
 } else {
     alert("Bienvenido a la web")
 }
+
+
+
+
+
+
 
