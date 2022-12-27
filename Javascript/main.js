9af35a4f3c78d66c1b53ad52c6d94fb9beceff68
Jarod Calderon <jarodcalde97@gmail.com>
2022-12-26 23:39:37 -0300

    preEntregaJS3

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,50 +1,50 @@
-const precio = 200;
-const tax = 0.19;
+console.log(precioTema[0] * tax);
+console.log(precioTema);
 
-let contratar = prompt('¿Quieres promocionar un tema con nosotros?')
+let iva = precioTema[0] * tax;
+let total = precioTema[0] + iva;
+alert(`el precio de promocion de tus productos es ${total}`);
+alert(`serás contactado al ${persona.telefono}`);
 
-if (contratar.toLowerCase() == 'si') {
-    const persona = {
-        nombre: String,
-        apellido: String,
-        telefono: parseInt,
-        cantidadTemas: parseInt
+function demo() {
+    let email = document.getElementById("email").value;
+
+    localStorage.setItem("email", email);
+
+    let nombrecompleto = document.getElementById("nombrecompleto").value;
+
+    localStorage.setItem("nombre completo", nombrecompleto);
+
+    let telefono = document.getElementById("telefono").value;
+
+    localStorage.setItem("telefono", telefono);
+
+    const precio = 200;
+
+    let temas = +document.getElementById("cTemas").value;
+
+    if (temas == 1) {
+
+        document.getElementById("precio").textContent = "El valor es" + precio;
+        document.getElementById("presupuesto").textContent = "seras contactado al " + localStorage.getItem('telefono');
+
+    } else if (temas == 2) {
+
+        document.getElementById("precio").textContent = "El valor es " + precio * 2;
+        document.getElementById("presupuesto").textContent = "seras contactado al " + localStorage.getItem('telefono');
+
+    } else if (temas == 3) {
+
+        document.getElementById("precio").textContent = "El valor es " + precio * 3;
+        document.getElementById("presupuesto").textContent = "seras contactado al " + localStorage.getItem('telefono');
+
+    } else {
+        alert("Consigue mas informacion en la web");
     }
 
-    let precios = [precio, precio * 2, precio * 3]
+    
 
-    persona.nombre = prompt("Cuál es tu nombre?")
-    persona.apellido = prompt("cuál es tu apellido?")
-    persona.telefono = prompt("Cuál es tu número telefónico")
-    persona.cantidadTemas = prompt("cuántos temas quieres promocionar, máximo 3")
+    document.getElementById("info2").innerHTML = localStorage.getItem('nombre completo');
+    document.getElementById("info3").innerHTML = localStorage.getItem('telefono');
 
-
-
-    const precioTema = precios.filter((precio,index) => {
-        if (
-            index == persona.cantidadTemas - 1
-        ) {
-            return true;
-        }
-        return false;
-    })
-
-    console.log(precioTema[0] * tax)
-    console.log(precioTema)
-
-    let iva = precioTema[0] * tax 
-    let total = precioTema[0] + iva
-    alert(`el precio de promocion de tus productos es ${total}`)
-    alert(`serás contactado al ${persona.telefono}`)
-
-
-} else {
-    alert("Bienvenido a la web")
 }
-
-
-
-
-
-
-
