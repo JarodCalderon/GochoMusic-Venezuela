f7382f34f36b2da96c0e7dcc997b10a094d906f5
Jarod Calderon <jarodcalde97@gmail.com>
2023-01-21 22:11:26 -0300

    proyectoFinal2

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,12 +1,22 @@
-console.log(precioTema[0] * tax);
-console.log(precioTema);
-
-let iva = precioTema[0] * tax;
-let total = precioTema[0] + iva;
-alert(`el precio de promocion de tus productos es ${total}`);
-alert(`serás contactado al ${persona.telefono}`);
-
 function demo() {
+    const Data = class {
+        constructor(email, nombrecompleto, telefono, temas) {
+          this.email = email;
+          this.nombrecompleto = nombrecompleto;
+          this.telefono = telefono;
+          this.temas = temas;
+          this.precio = 200;
+        }
+
+        get price() {
+            return this.calcPrice();
+        }
+
+        calcPrice() {
+        return this.temas * this.precio;
+        }
+    };
+
     let email = document.getElementById("email").value;
 
     localStorage.setItem("email", email);
@@ -17,25 +27,42 @@ function demo() {
 
     let telefono = document.getElementById("telefono").value;
 
+    // Validacion de telefono
+    if (telefono.length > 11 || telefono.length < 11) {
+        Toastify({
+            text: "El telefono debe tener 11 digitos!",
+            className: "error",
+            duration: 3000,
+            style: {
+                background: "linear-gradient(to right, #cb3234, #ff0000)",
+            },
+            offset: {
+                x: 50, 
+                y: 50
+              },
+            }).showToast();
+        return 0;
+    }
+
     localStorage.setItem("telefono", telefono);
 
-    const precio = 200;
+    let temas =+ document.getElementById("cTemas").value;
 
-    let temas = +document.getElementById("cTemas").value;
+    const informacion = new Data(email, nombrecompleto, telefono, temas);
 
-    if (temas == 1) {
+    if (informacion.temas == 1) {
 
-        document.getElementById("precio").textContent = "El valor es" + precio;
+        document.getElementById("precio").textContent = "El valor es " + informacion.price;
         document.getElementById("presupuesto").textContent = "seras contactado al " + localStorage.getItem('telefono');
 
-    } else if (temas == 2) {
+    } else if (informacion.temas == 2) {
 
-        document.getElementById("precio").textContent = "El valor es " + precio * 2;
+        document.getElementById("precio").textContent = "El valor es " + informacion.price;
         document.getElementById("presupuesto").textContent = "seras contactado al " + localStorage.getItem('telefono');
 
-    } else if (temas == 3) {
+    } else if (informacion.temas == 3) {
 
-        document.getElementById("precio").textContent = "El valor es " + precio * 3;
+        document.getElementById("precio").textContent = "El valor es " + informacion.price;
         document.getElementById("presupuesto").textContent = "seras contactado al " + localStorage.getItem('telefono');
 
     } else {
@@ -47,6 +74,30 @@ function demo() {
     document.getElementById("info2").innerHTML = localStorage.getItem('nombre completo');
     document.getElementById("info3").innerHTML = localStorage.getItem('telefono');
 
+    const ul = document.getElementById('authors');
+    const url = 'https://randomuser.me/api/?results=5';
+
+    fetch(url)
+        .then((resp) => resp.json())
+        .then(function (data) {
+            let authors = data.results;
+            document.getElementById("recomendacion").textContent = "Te recomendamos seguir algunos de nuestros autores:  ";
+
+            return authors.map(function (author) {
+                let li = createNode('li');
+                let img = createNode('img');
+                let span = createNode('span');
+                img.src = author.picture.medium;
+                span.innerHTML = `${ author.name.first } ${ author.name.last }`;
+                append(li, img);
+                append(li, span);
+                append(ul, li);
+            })
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
 }
 
 function createNode(element) {
@@ -56,25 +107,3 @@ function createNode(element) {
 function append(parent, el) {
     return parent.appendChild(el);
 }
-
-const ul = document.getElementById('authors');
-const url = 'https://randomuser.me/api/?results=10';
-
-fetch(url)
-    .then((resp) => resp.json())
-    .then(function (data) {
-        let authors = data.results;
-        return authors.map(function (author) {
-            let li = createNode('li');
-            let img = createNode('img');
-            let span = createNode('span');
-            img.src = author.picture.medium;
-            span.innerHTML = `${ author.name.first } ${ author.name.last }`;
-            append(li, img);
-            append(li, span);
-            append(ul, li);
-        })
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
